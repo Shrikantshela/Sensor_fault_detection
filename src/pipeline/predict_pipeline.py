8a91df07d05aaca67f53fedebc93e8574b7b2f87
Shrikant Shelar <99086180+Shrikantshela@users.noreply.github.com>
2023-12-14 14:44:48 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -46,6 +46,7 @@ class PredictionPipeline:
         """
 
         try:
+            #creating a file  
             pred_file_input_dir = "prediction_artifacts"
             os.makedirs(pred_file_input_dir, exist_ok=True)
 
